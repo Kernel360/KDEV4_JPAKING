2d4bdb3552a255b3125b35cd838f31bc0578b6d6
ijjung365 <ijjung365@gmail.com>
2025-02-26 18:00:26 +0900

    실습완성

---
+++ src/main/java/org/example/web/TaskController.java
@@ -0,0 +1,86 @@
+package org.example.web;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.constants.TaskStatus;
+import org.example.model.Task;
+import org.example.service.TaskService;
+import org.example.web.vo.ResultResponse;
+import org.example.web.vo.TaskRequest;
+import org.example.web.vo.TaskStatusRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Controller
+@RequestMapping("/tasks")
+@RequiredArgsConstructor
+public class TaskController {
+
+    private final TaskService taskService;
+
+    @PostMapping
+    public ResponseEntity<Task> createTask(@RequestBody TaskRequest req) {
+        var result = taskService.add(req.getTitle(), req.getDescription(), req.getDueDate());
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Task>> getTask(Optional<String> dueDate) {
+        List<Task> result;
+
+        if (dueDate.isPresent()) {
+            result = this.taskService.getByDueDate(dueDate.get());
+        } else {
+            result = this.taskService.getAll();
+        }
+
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> fetchOneTask(@PathVariable Long id) {
+        var result = this.taskService.getOne(id);
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/status/{status}")
+    public ResponseEntity<List<Task>> getByStatus(@PathVariable TaskStatus status) {
+        var result = this.taskService.getByStatus(status);
+        return ResponseEntity.ok(result);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> updateTask(@PathVariable Long id,
+                                           @RequestBody TaskRequest task) {
+        var result = this.taskService.update(id,
+                task.getTitle(),
+                task.getDescription(),
+                task.getDueDate());
+        return ResponseEntity.ok(result);
+    }
+
+    @PatchMapping("/{id}/status")
+    public ResponseEntity<Task> updateTaskStatus(@PathVariable Long id,
+                                                 @RequestBody TaskStatusRequest req) {
+        var result = this.taskService.updateStatus(id, req.getStatus());
+        return ResponseEntity.ok(result);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ResultResponse> deleteTask(@PathVariable Long id) {
+        var result = this.taskService.delete(id);
+        return ResponseEntity.ok(new ResultResponse(result));
+    }
+
+    @GetMapping("/status" )
+    public ResponseEntity<TaskStatus[]> getAllStatus() {
+        var status = TaskStatus.values();
+        return ResponseEntity.ok(status);
+    }
+
+}
